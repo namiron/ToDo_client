585bf127efec24e85a1e037ec2fa961c30ab07cb
namiron <sasha_zpua@mail.ru>
2024-08-26 17:33:52 +0300

    nav

--- src/redux/API/user/useAuthorization.ts
+++ src/redux/API/user/useAuthorization.ts
@@ -1,24 +1,26 @@
-import { createAsyncThunk, } from '@reduxjs/toolkit';
-import axios from 'axios';
-import { AppDispatch } from '../../store';
-import { isUserAuth } from '../../reducers/userSlice';
-import { IUserType } from '../../types/IUserProps';
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { AppDispatch } from "../../store";
+import { isUserAuth } from "../../reducers/userSlice";
+import { IUserType } from "../../types/IUserProps";
 
 const baseUrl = import.meta.env.VITE_BASE_URL;
 
-export const useAuthorization = createAsyncThunk<IUserType, void, { dispatch: AppDispatch, rejectWithValue: string }>(
-    'user/useAuthorization',
-    async (_, { dispatch, rejectWithValue }) => {
-        try {
-            const response = await axios.get(`${baseUrl}/auth/authorization`, {
-                headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
-            })
+export const useAuthorization = createAsyncThunk<
+  IUserType,
+  void,
+  { dispatch: AppDispatch; rejectWithValue: string }
+>("user/useAuthorization", async (_, { dispatch, rejectWithValue }) => {
+  try {
+    const response = await axios.get(`${baseUrl}/auth/authorization`, {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+    });
 
-            const data: IUserType = await response.data
-            dispatch(isUserAuth(data))
-            return data
-        } catch (error) {
-            return rejectWithValue('authorization error')
-        }
-    }
-)
+    const data: IUserType = await response.data;
+    dispatch(isUserAuth(data));
+    console.log("user", data);
+    return data;
+  } catch (error) {
+    return rejectWithValue("authorization error");
+  }
+});
--- src/redux/API/user/userRegistration.ts
+++ src/redux/API/user/userRegistration.ts
@@ -1,35 +1,44 @@
-import { createAsyncThunk, } from '@reduxjs/toolkit';
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
-import axios from 'axios';
-import { IRegistrationTypes, IUserType } from '../../types/IUserProps';
-import { AppDispatch } from '../../store';
-import { isUserAuth } from '../../reducers/userSlice';
-
-
+import axios from "axios";
+import { IRegistrationTypes, IUserType } from "../../types/IUserProps";
+import { AppDispatch } from "../../store";
+import { isUserAuth } from "../../reducers/userSlice";
 
 const baseUrl = import.meta.env.VITE_BASE_URL;
 
-
-export const useRegistration = createAsyncThunk<IUserType, IRegistrationTypes, { dispatch: AppDispatch, rejectWidthValue: string }>(
-    'user/useRegistration',
-    async ({ email, name, surname, password }, { dispatch, rejectWithValue, }) => {
-        try {
-            const response = await axios.post(`${baseUrl}/auth/registration`, {
-                email, name, surname, password
-            }, {
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            })
-            if (!response.data) {
-                return rejectWithValue('no data')
-            }
-            const data: IUserType = await response.data
-
-            dispatch(isUserAuth(data))
-            return data
-        } catch (error) {
-            return rejectWithValue('registration error')
+export const useRegistration = createAsyncThunk<
+  IUserType,
+  IRegistrationTypes,
+  { dispatch: AppDispatch; rejectWidthValue: string }
+>(
+  "user/useRegistration",
+  async ({ email, name, surname, password }, { dispatch, rejectWithValue }) => {
+ 
+    try {
+      const response = await axios.post(
+        `${baseUrl}/auth/registration`,
+        {
+          email,
+          name,
+          surname,
+          password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
+      );
+      if (!response.data) {
+        return rejectWithValue("no data");
+      }
+      const data: IUserType = await response.data;
+
+      dispatch(isUserAuth(data));
+      return data;
+    } catch (error) {
+      return rejectWithValue("registration error");
     }
-)
+  }
+);
